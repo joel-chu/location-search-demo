6e88abfaf3393be9374c0b4b9252870ecd52e6a0
Joel Chu <joelckchu@gmail.com>
2021-06-01 19:26:37 +0800

    update with the static serve

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,10 +1,14 @@
 // src/app.ts
 // DON'T USE import * as Koa from 'koa'! it will crash
-
+import { join } from 'path'
 import Koa from 'koa'
+import serve from 'koa-static'
 import HttpStatus from 'http-status-codes'
 import router from './router'
+
 const app:Koa = new Koa()
+
+const buildDir = join(__dirname, '..', '..', 'demo-ui', 'build')
 
 // Generic error handling middleware.
 app.use(async (ctx: Koa.Context, next: () => Promise<any>) => {
@@ -17,6 +21,8 @@ app.use(async (ctx: Koa.Context, next: () => Promise<any>) => {
     ctx.app.emit('error', error, ctx)
   }
 })
+
+app.use(serve(buildDir))
 
 app.use(router.routes())
 app.use(router.allowedMethods())
