b7505c831d050739bae520ef01a9e631611cc8b5
Joel Chu <joelckchu@gmail.com>
2021-06-01 19:21:50 +0800

    add build

--- demo-ui/preact.config.js
+++ demo-ui/preact.config.js
@@ -1,31 +1,33 @@
 // preact.config.js
 // need to proxy back to our Koa setup
 module.exports = function(config) {
-  config.devServer.proxy = [
-    {
-      // proxy requests matching a pattern:
-      path: '/query/**',
-      // where to proxy to:
-      target: 'http://localhost:3388/query',
-      // optionally change Origin: and Host: headers to match target:
-      changeOrigin: true,
-      changeHost: true,
+  if (config.devServer) {
+    config.devServer.proxy = [
+      {
+        // proxy requests matching a pattern:
+        path: '/query/**',
+        // where to proxy to:
+        target: 'http://localhost:3388/query',
+        // optionally change Origin: and Host: headers to match target:
+        changeOrigin: true,
+        changeHost: true,
 
-      // optionally mutate request before proxying:
-      pathRewrite: function(path, req) {
-        // you can modify the outbound proxy request here:
-        delete req.headers.referer;
+        // optionally mutate request before proxying:
+        pathRewrite: function(path, req) {
+          // you can modify the outbound proxy request here:
+          delete req.headers.referer;
 
-        // common: remove first path segment: (/api/**)
-        return '/' + path.replace(/^\/[^\/]+\//, '');
-      },
+          // common: remove first path segment: (/api/**)
+          return '/' + path.replace(/^\/[^\/]+\//, '');
+        },
 
-      // optionally mutate proxy response:
-      onProxyRes: function(proxyRes, req, res) {
-        // you can modify the response here:
-        proxyRes.headers.connection = 'keep-alive';
-        proxyRes.headers['cache-control'] = 'no-cache';
+        // optionally mutate proxy response:
+        onProxyRes: function(proxyRes, req, res) {
+          // you can modify the response here:
+          proxyRes.headers.connection = 'keep-alive';
+          proxyRes.headers['cache-control'] = 'no-cache';
+        }
       }
-    }
-  ];
-};
+    ]
+  }
+}
