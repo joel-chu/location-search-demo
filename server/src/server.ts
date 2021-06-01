5f8b0eb18e19aba8f16406f2fa0c6e65b9916d5d
Joel Chu <joelckchu@gmail.com>
2021-06-01 22:19:25 +0800

    update the server started message

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,8 +4,8 @@ import app from './app'
 
 // Process.env will always be comprised of strings, so we typecast the port to a
 // number.
-const PORT:number = Number(process.env.PORT) || 3388
+const PORT: number = Number(process.env.PORT) || 3388
 
 app.listen(PORT, () => {
-  console.log(`Server started on ${PORT} point your brower at http://localhost:3388`)
+  console.log(`Server started on ${PORT} point your brower at http://localhost:${PORT}`)
 })
