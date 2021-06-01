3e4c2902c0388ee6f22dc0e948d7e37246aa2f4b
Joel Chu <joelckchu@gmail.com>
2021-06-01 21:03:30 +0800

    remove debug code from searchbox comp

--- demo-ui/src/components/searchbox/index.tsx
+++ demo-ui/src/components/searchbox/index.tsx
@@ -37,8 +37,6 @@ class SearchBox extends Component {
       <div class={style.searchBox}>
         <form onSubmit={this.onSubmit}>
           <input type="text" value={opt.value} onInput={this.onInput} />
-
-          <p>You typed this value: {opt.value}</p>
           <button type="submit" disabled={opt.value.length <= 1}>Search</button>
           <button type="button" onClick={this.reset} disabled={!(this.state.result.length > 0)}>RESET</button>
         </form>
@@ -54,7 +52,7 @@ class SearchBox extends Component {
     )
   }
 }
-// not in use 
+// not in use
 // <div class={ style.ldsRing }><div></div><div></div><div></div><div></div></div>
 
 export default SearchBox
